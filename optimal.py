b36d4698a00d0c05ad2f57407d2e808ab013200e
Dylan Richardson <dylanrichardson1996@gmail.com>
2018-01-31 14:17:34 -0500

    Reformat global parameters. Save optimal trades to file

--- optimal.py
+++ optimal.py
@@ -1,11 +1,72 @@
 from __future__ import (absolute_import, division, print_function, unicode_literals)
+import os
+import json
+from params import PARAMS
+from utility import *
+from data import get_symbol_data
 
 
 BUY = 1
 SELL = -1
 
 
-def get_optimal_trades(data, tolerance):
+def get_optimal_trades(symbol, start, end, tolerance):
+    path = get_path(symbol, start, end, tolerance)
+    trades = retrieve_trades(path)
+    if trades:
+        return trades
+    data = get_close_data(symbol, start, end)
+    trades = calc_trades(data, tolerance)
+    save_trades(trades, path)
+    return trades
+
+
+def get_path(symbol, start, end, tolerance):
+    file_name = hash_dict({
+        'symbol': symbol,
+        'start': start,
+        'end': end,
+        'tolerance': tolerance
+    })
+    cwd = os.getcwd()
+    return os.path.join(cwd, 'data', 'optimal', file_name + '.json')
+
+
+def retrieve_trades(path):
+    try:
+        with open(path, 'r') as f:
+            return json.load(f)
+    except FileNotFoundError:
+        return {}
+
+
+def save_trades(trades, path):
+    if not os.path.exists(os.path.dirname(path)):
+        os.makedirs(os.path.dirname(path))
+    with open(path, 'w') as outfile:
+        json.dump(trades, outfile)
+
+
+def get_close_data(symbol, start, end):
+    daily_options = PARAMS['data_options'][0]
+    data = get_symbol_data([daily_options], symbol)
+    data = filter_dates(data, start, end)
+    data = filter_close(data)
+    return data
+
+
+def filter_dates(data, start, end):
+    return {
+        date: val for date, val in data.items() if date_between(date, start, end)
+    }
+
+
+def filter_close(data):
+    close_hash = get_close_hash()
+    return { date: float(columns[close_hash]) for date, columns in data.items() }
+
+
+def calc_trades(data, tolerance):
     dates = sorted(data)
     prices = [ data[date] for date in dates ]
     trades = optimize_trades(prices, tolerance)
@@ -23,18 +84,18 @@ def optimize_trades(prices, tolerance):
 
     # determine whether to buy or sell first
     for index, price in enumerate(prices[1:]):
-        index = index - delay # index is behind by one = index - 1
+        index -= delay # index is behind by one = index - 1
         price_diff = (price - prices[index]) / prices[index]
 
         if 0 <= price_diff and price_diff <= tolerance:
-            delay = delay + 1
+            delay += 1
         else:
             delay = 0
             if price_diff > 0:
                 buying = True
                 break
         if -tolerance <= price_diff and price_diff < 0:
-            delay = delay + 1
+            delay += 1
         else:
             delay = 0
             if price_diff < 0:
@@ -44,12 +105,12 @@ def optimize_trades(prices, tolerance):
 
     # determine when to buy and sell
     for index, price in enumerate(prices[1:]):
-        index = index - delay # index is behind by one = index - 1
+        index -= delay # index is behind by one = index - 1
         price_diff = (price - prices[index]) / prices[index]
 
         if buying: # looking to buy
             if 0 <= price_diff and price_diff <= tolerance:
-                delay = delay + 1
+                delay += 1
             else:
                 delay = 0
                 if price_diff > 0:
@@ -57,7 +118,7 @@ def optimize_trades(prices, tolerance):
                     buying = False
         else: # looking to sell
             if -tolerance <= price_diff and price_diff <= 0:
-                delay = delay + 1
+                delay += 1
             else:
                 delay = 0
                 if price_diff < 0:
@@ -65,3 +126,5 @@ def optimize_trades(prices, tolerance):
                     buying = True
 
     return trades
+
+get_optimal_trades('AAPL', '2018-01-01', '2018-01-30', 0.01)
--- params.py
+++ params.py
@@ -1,230 +1,233 @@
 import os
 
-# credentials
+PARAMS = {
 
-API_KEY = os.environ['API_KEY']
-USERNAME = os.environ['USERNAME']
-PASSWORD = os.environ['PASSWORD']
+    'credentials': {
+        'alphavantage': os.environ['alphavantage'],
+        'intrinio': {
+            'username': os.environ['intrinio_username'],
+            'password': os.environ['intrinio_password']
+        }
+    },
 
-# stock selection
+    'screeners': {
+        'yahoo': [
+            'undervalued_growth_stocks',
+            'day_gainers',
+            'day_losers',
+            'most_actives',
+            'growth_technology_stocks',
+            'undervalued_large_caps',
+            'aggressive_small_caps',
+            'portfolio_anchors',
+            'solid_large_growth_funds'
+        ],
+        'intrinio': {
+            'undervalued': {
+                'conditions': [
+                    ['pricetoearnings', '<=', 20],
+                    ['pricetoearnings', '>', 0]
+                ]
+            }
+        }
+    },
 
-# yahoo screener names
-PORTFOLIO_LIST = [
-    'undervalued_growth_stocks',
-    'day_gainers',
-    'day_losers',
-    'most_actives',
-    'growth_technology_stocks',
-    'undervalued_large_caps',
-    'aggressive_small_caps',
-    'portfolio_anchors',
-    'solid_large_growth_funds'
-]
+    'data_options': [{
+        'function': 'TIME_SERIES_DAILY',
+        'columns': ['open', 'high', 'low', 'close', 'volume'],
+        'outputsize': 'full'
+    }, {
+        'function': 'TIME_SERIES_DAILY_ADJUSTED',
+        'columns': ['adjusted close', 'dividend amount', 'split coefficient'],
+        'outputsize': 'full'
+    }, {
+        'function': 'SMA',
+        'columns': ['SMA'],
+        'interval': 'daily',
+        'time_period': '50',
+        'series_type': 'close'
+    }, {
+        'function': 'SMA',
+        'columns': ['SMA'],
+        'interval': 'daily',
+        'time_period': '200',
+        'series_type': 'close'
+    }, {
+        'function': 'EMA',
+        'columns': ['EMA'],
+        'interval': 'daily',
+        'time_period': '50',
+        'series_type': 'close'
+    }, {
+        'function': 'EMA',
+        'columns': ['EMA'],
+        'interval': 'daily',
+        'time_period': '200',
+        'series_type': 'close'
+    }, {
+        'function': 'MACD',
+        'columns': ['MACD_Signal', 'MACD_Hist', 'MACD'],
+        'interval': 'daily',
+        'series_type': 'close',
+        'fastperiod': '12',
+        'slowperiod': '26',
+        'signalperiod': '9',
+    }, {
+        'function': 'MACD',
+        'columns': ['MACD_Signal', 'MACD_Hist', 'MACD'],
+        'interval': 'daily',
+        'series_type': 'close',
+        'fastperiod': '5',
+        'slowperiod': '35',
+        'signalperiod': '5',
+    }, {
+        'function': 'STOCH',
+        'columns': ['SlowD', 'SlowK'],
+        'interval': 'daily',
+        'series_type': 'close',
+        'fastkperiod': '5',
+        'slowkperiod': '3',
+        'slowdperiod': '3',
+        'slowkmatype': '0',
+        'slowdmatype': '0'
+    }, {
+        'function': 'STOCH',
+        'columns': ['SlowD', 'SlowK'],
+        'interval': 'daily',
+        'series_type': 'close',
+        'fastkperiod': '5',
+        'slowkperiod': '3',
+        'slowdperiod': '3',
+        'slowkmatype': '1',
+        'slowdmatype': '1'
+    }, {
+        'function': 'STOCH',
+        'columns': ['SlowD', 'SlowK'],
+        'interval': 'daily',
+        'series_type': 'close',
+        'fastkperiod': '5',
+        'slowkperiod': '3',
+        'slowdperiod': '3',
+        'slowkmatype': '2',
+        'slowdmatype': '2'
+    }, {
+        'function': 'STOCH',
+        'columns': ['SlowD', 'SlowK'],
+        'interval': 'daily',
+        'series_type': 'close',
+        'fastkperiod': '5',
+        'slowkperiod': '3',
+        'slowdperiod': '3',
+        'slowkmatype': '7',
+        'slowdmatype': '7'
+    }, {
+        'function': 'STOCH',
+        'columns': ['SlowD', 'SlowK'],
+        'interval': 'daily',
+        'series_type': 'close',
+        'fastkperiod': '5',
+        'slowkperiod': '3',
+        'slowdperiod': '3',
+        'slowkmatype': '8',
+        'slowdmatype': '8'
+    }, {
+        'function': 'RSI',
+        'columns': ['RSI'],
+        'interval': 'daily',
+        'series_type': 'close',
+        'time_period': '14'
+    }, {
+        'function': 'RSI',
+        'columns': ['RSI'],
+        'interval': 'daily',
+        'series_type': 'close',
+        'time_period': '60'
+    }, {
+        'function': 'ADX',
+        'columns': ['ADX'],
+        'interval': 'daily',
+        'series_type': 'close',
+        'time_period': '14'
+    }, {
+        'function': 'ADX',
+        'columns': ['ADX'],
+        'interval': 'daily',
+        'series_type': 'close',
+        'time_period': '60'
+    }, {
+        'function': 'CCI',
+        'columns': ['CCI'],
+        'interval': 'daily',
+        'series_type': 'close',
+        'time_period': '20'
+    }, {
+        'function': 'CCI',
+        'columns': ['CCI'],
+        'interval': 'daily',
+        'time_period': '60'
+    }, {
+        'function': 'AROON',
+        'columns': ['Aroon Up', 'Aroon Down'],
+        'interval': 'daily',
+        'time_period': '14'
+    }, {
+        'function': 'AROON',
+        'columns': ['Aroon Up', 'Aroon Down'],
+        'interval': 'daily',
+        'time_period': '60'
+    }, {
+        'function': 'BBANDS',
+        'columns': ['Real Middle Band', 'Real Upper Band', 'Real Lower Band'],
+        'interval': 'daily',
+        'time_period': '14',
+        'series_type': 'close',
+        'nbdevup': '2',
+        'nbdevdn': '2',
+        'matype': '0'
+    }, {
+        'function': 'BBANDS',
+        'columns': ['Real Middle Band', 'Real Upper Band', 'Real Lower Band'],
+        'interval': 'daily',
+        'time_period': '14',
+        'series_type': 'close',
+        'nbdevup': '2',
+        'nbdevdn': '2',
+        'matype': '1'
+    }, {
+        'function': 'BBANDS',
+        'columns': ['Real Middle Band', 'Real Upper Band', 'Real Lower Band'],
+        'interval': 'daily',
+        'time_period': '14',
+        'series_type': 'close',
+        'nbdevup': '2',
+        'nbdevdn': '2',
+        'matype': '2'
+    }, {
+        'function': 'BBANDS',
+        'columns': ['Real Middle Band', 'Real Upper Band', 'Real Lower Band'],
+        'interval': 'daily',
+        'time_period': '14',
+        'series_type': 'close',
+        'nbdevup': '2',
+        'nbdevdn': '2',
+        'matype': '7'
+    }, {
+        'function': 'BBANDS',
+        'columns': ['Real Middle Band', 'Real Upper Band', 'Real Lower Band'],
+        'interval': 'daily',
+        'time_period': '14',
+        'series_type': 'close',
+        'nbdevup': '2',
+        'nbdevdn': '2',
+        'matype': '8'
+    }, {
+        'function': 'AD',
+        'columns': ['Chaikin A/D'],
+        'interval': 'daily',
+    }, {
+        'function': 'OBV',
+        'columns': ['OBV'],
+        'interval': 'daily',
+    }]
 
-# example of Intrinio conditions
-UNDERVALUED = {
-    'conditions': [
-        ['pricetoearnings', '<=', 20],
-        ['pricetoearnings', '>', 0]
-    ]
 }
-
-# data selection
-
-DATA_OPTIONS_LIST = [{
-    'function': 'TIME_SERIES_DAILY',
-    'columns': ['open', 'high', 'low', 'close', 'volume'],
-    'outputsize': 'full'
-}, {
-    'function': 'TIME_SERIES_DAILY_ADJUSTED',
-    'columns': ['open', 'high', 'low', 'close', 'adjusted close', 'volume', 'dividend amount', 'split coefficient'],
-    'outputsize': 'full'
-}, {
-    'function': 'SMA',
-    'columns': ['SMA'],
-    'interval': 'daily',
-    'time_period': '50',
-    'series_type': 'close'
-}, {
-    'function': 'SMA',
-    'columns': ['SMA'],
-    'interval': 'daily',
-    'time_period': '200',
-    'series_type': 'close'
-}, {
-    'function': 'EMA',
-    'columns': ['EMA'],
-    'interval': 'daily',
-    'time_period': '50',
-    'series_type': 'close'
-}, {
-    'function': 'EMA',
-    'columns': ['EMA'],
-    'interval': 'daily',
-    'time_period': '200',
-    'series_type': 'close'
-}, {
-    'function': 'MACD',
-    'columns': ['MACD_Signal', 'MACD_Hist', 'MACD'],
-    'interval': 'daily',
-    'series_type': 'close',
-    'fastperiod': '12',
-    'slowperiod': '26',
-    'signalperiod': '9',
-}, {
-    'function': 'MACD',
-    'columns': ['MACD_Signal', 'MACD_Hist', 'MACD'],
-    'interval': 'daily',
-    'series_type': 'close',
-    'fastperiod': '5',
-    'slowperiod': '35',
-    'signalperiod': '5',
-}, {
-    'function': 'STOCH',
-    'columns': ['SlowD', 'SlowK'],
-    'interval': 'daily',
-    'series_type': 'close',
-    'fastkperiod': '5',
-    'slowkperiod': '3',
-    'slowdperiod': '3',
-    'slowkmatype': '0',
-    'slowdmatype': '0'
-}, {
-    'function': 'STOCH',
-    'columns': ['SlowD', 'SlowK'],
-    'interval': 'daily',
-    'series_type': 'close',
-    'fastkperiod': '5',
-    'slowkperiod': '3',
-    'slowdperiod': '3',
-    'slowkmatype': '1',
-    'slowdmatype': '1'
-}, {
-    'function': 'STOCH',
-    'columns': ['SlowD', 'SlowK'],
-    'interval': 'daily',
-    'series_type': 'close',
-    'fastkperiod': '5',
-    'slowkperiod': '3',
-    'slowdperiod': '3',
-    'slowkmatype': '2',
-    'slowdmatype': '2'
-}, {
-    'function': 'STOCH',
-    'columns': ['SlowD', 'SlowK'],
-    'interval': 'daily',
-    'series_type': 'close',
-    'fastkperiod': '5',
-    'slowkperiod': '3',
-    'slowdperiod': '3',
-    'slowkmatype': '7',
-    'slowdmatype': '7'
-}, {
-    'function': 'STOCH',
-    'columns': ['SlowD', 'SlowK'],
-    'interval': 'daily',
-    'series_type': 'close',
-    'fastkperiod': '5',
-    'slowkperiod': '3',
-    'slowdperiod': '3',
-    'slowkmatype': '8',
-    'slowdmatype': '8'
-}, {
-    'function': 'RSI',
-    'columns': ['RSI'],
-    'interval': 'daily',
-    'series_type': 'close',
-    'time_period': '14'
-}, {
-    'function': 'RSI',
-    'columns': ['RSI'],
-    'interval': 'daily',
-    'series_type': 'close',
-    'time_period': '60'
-}, {
-    'function': 'ADX',
-    'columns': ['ADX'],
-    'interval': 'daily',
-    'series_type': 'close',
-    'time_period': '14'
-}, {
-    'function': 'ADX',
-    'columns': ['ADX'],
-    'interval': 'daily',
-    'series_type': 'close',
-    'time_period': '60'
-}, {
-    'function': 'CCI',
-    'columns': ['CCI'],
-    'interval': 'daily',
-    'series_type': 'close',
-    'time_period': '20'
-}, {
-    'function': 'CCI',
-    'columns': ['CCI'],
-    'interval': 'daily',
-    'time_period': '60'
-}, {
-    'function': 'AROON',
-    'columns': ['Aroon Up', 'Aroon Down'],
-    'interval': 'daily',
-    'time_period': '14'
-}, {
-    'function': 'AROON',
-    'columns': ['Aroon Up', 'Aroon Down'],
-    'interval': 'daily',
-    'time_period': '60'
-}, {
-    'function': 'BBANDS',
-    'columns': ['Real Middle Band', 'Real Upper Band', 'Real Lower Band'],
-    'interval': 'daily',
-    'time_period': '14',
-    'series_type': 'close',
-    'nbdevup': '2',
-    'nbdevdn': '2',
-    'matype': '0'
-}, {
-    'function': 'BBANDS',
-    'columns': ['Real Middle Band', 'Real Upper Band', 'Real Lower Band'],
-    'interval': 'daily',
-    'time_period': '14',
-    'series_type': 'close',
-    'nbdevup': '2',
-    'nbdevdn': '2',
-    'matype': '1'
-}, {
-    'function': 'BBANDS',
-    'columns': ['Real Middle Band', 'Real Upper Band', 'Real Lower Band'],
-    'interval': 'daily',
-    'time_period': '14',
-    'series_type': 'close',
-    'nbdevup': '2',
-    'nbdevdn': '2',
-    'matype': '2'
-}, {
-    'function': 'BBANDS',
-    'columns': ['Real Middle Band', 'Real Upper Band', 'Real Lower Band'],
-    'interval': 'daily',
-    'time_period': '14',
-    'series_type': 'close',
-    'nbdevup': '2',
-    'nbdevdn': '2',
-    'matype': '7'
-}, {
-    'function': 'BBANDS',
-    'columns': ['Real Middle Band', 'Real Upper Band', 'Real Lower Band'],
-    'interval': 'daily',
-    'time_period': '14',
-    'series_type': 'close',
-    'nbdevup': '2',
-    'nbdevdn': '2',
-    'matype': '8'
-}, {
-    'function': 'AD',
-    'columns': ['Chaikin A/D'],
-    'interval': 'daily',
-}, {
-    'function': 'OBV',
-    'columns': ['OBV'],
-    'interval': 'daily',
-}]
